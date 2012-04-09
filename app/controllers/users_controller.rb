df417dccfdc65449531d1a13012d230c46488b79
David Holland <dah_7@yahoo.com>
2012-04-09 10:51:09 -0400

    Now new users can sign up!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate
+  before_filter :authenticate, :only => [:edit, :update]
   
   def show
 	@user = User.find(params[:id])
