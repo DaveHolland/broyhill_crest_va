c09d0c65a73119e2ef08e57532bb857773ac5017
David Holland <dah_7@yahoo.com>
2012-04-21 16:42:00 -0400

    Initial Demo Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BroyhillCrestVa::Application.config.secret_token = '8ccfe868ba707b272a50eaecf24af5ecd992036830cc42bc99d33529d2103a3a664161b5a6af2fc25e63902a934b0451d13f6f027452065b40edb777b25dde5b'
