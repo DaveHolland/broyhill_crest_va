84d14d65c06daa2dd3503302da4add7b1f183968
David Holland <dah_7@yahoo.com>
2012-04-21 15:58:00 -0400

    BCCA web site 1st iteration

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,19 @@ SampleApp::Application.routes.draw do
 	resources :users
 	resources :sessions, :only => [:new, :create, :destroy]
 	
-	match '/signup', :to => 'users#new'
-	match '/signin', 	:to => 'sessions#new'
-	match '/signout',	:to => 'sessions#destroy'
+	match '/signup', 		:to => 'users#new'
+	match '/signin', 		:to => 'sessions#new'
+	match '/signout',		:to => 'sessions#destroy'
 	
-	match '/contact', 	:to => 'pages#contact'
-	match '/about', 	:to => 'pages#about'
-	match '/help', 		:to => 'pages#help'
+	match '/contact', 		:to => 'pages#contact'
+	match '/about', 		:to => 'pages#about'
+	match '/help', 			:to => 'pages#help'
+	match '/directory', 	:to=>  'pages#directory'
+	match '/n2n',			:to=>  'pages#n2n'
+	match '/watch', 		:to => 'pages#watch'
+	match '/hospitality', 	:to => 'pages#hospitality'
+	match '/schools', 		:to=>  'pages#schools'
+	match '/weather',		:to=>  'pages#weather'
 	
 	root :to => 'pages#home'
 	
